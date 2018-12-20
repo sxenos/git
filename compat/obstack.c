6c023d22fd44778a861045ff3af982b5f9c75dde
SZEDER Gábor <szeder.dev@gmail.com>
2018-12-20 17:24:48 +0100

    compat/obstack: fix -Wcast-function-type warnings
    When building Git with GCC 8.2.0 (at least from Homebrew on macOS,
    DEVELOPER flags enabled) one is greeted with a screenful of compiler
    errors:
    
      compat/obstack.c: In function '_obstack_begin':
      compat/obstack.c:162:17: error: cast between incompatible function types from 'void * (*)(long int)' to 'struct _obstack_chunk * (*)(void *, long int)' [-Werror=cast-function-type]
         h->chunkfun = (struct _obstack_chunk * (*)(void *, long)) chunkfun;
                       ^
      compat/obstack.c:163:16: error: cast between incompatible function types from 'void (*)(void *)' to 'void (*)(void *, struct _obstack_chunk *)' [-Werror=cast-function-type]
         h->freefun = (void (*) (void *, struct _obstack_chunk *)) freefun;
                      ^
      compat/obstack.c:116:8: error: cast between incompatible function types from 'struct _obstack_chunk * (*)(void *, long int)' to 'struct _obstack_chunk * (*)(long int)' [-Werror=cast-function-type]
          : (*(struct _obstack_chunk *(*) (long)) (h)->chunkfun) ((size)))
              ^
      compat/obstack.c:168:22: note: in expansion of macro 'CALL_CHUNKFUN'
         chunk = h->chunk = CALL_CHUNKFUN (h, h -> chunk_size);
                            ^~~~~~~~~~~~~
      <snip>
    
    'struct obstack' stores pointers to two functions to allocate and free
    "chunks", and depending on how obstack is used, these functions take
    either one parameter (like standard malloc() and free() do; this is
    how we use it) or two parameters.  Presumably to reduce memory
    footprint, a single field is used to store the function pointer for
    both signatures, and then it's casted to the appropriate signature
    when the function pointer is accessed.  These casts between function
    pointers with different number of parameters are what trigger those
    compiler errors.
    
    Modify 'struct obstack' to use unions to store function pointers with
    different signatures, and then use the union member with the
    appropriate signature when accessing these function pointers.  This
    eliminates the need for those casts, and thus avoids this compiler
    error.
    
    Signed-off-by: SZEDER Gábor <szeder.dev@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- compat/obstack.c
+++ compat/obstack.c
@@ -112,15 +112,15 @@ compat_symbol (libc, _obstack_compat, _obstack, GLIBC_2_0);
 
 # define CALL_CHUNKFUN(h, size) \
   (((h) -> use_extra_arg) \
-   ? (*(h)->chunkfun) ((h)->extra_arg, (size)) \
-   : (*(struct _obstack_chunk *(*) (long)) (h)->chunkfun) ((size)))
+   ? (*(h)->chunkfun.fn_extra_arg) ((h)->extra_arg, (size)) \
+   : (*(h)->chunkfun.fn) ((size)))
 
 # define CALL_FREEFUN(h, old_chunk) \
   do { \
     if ((h) -> use_extra_arg) \
-      (*(h)->freefun) ((h)->extra_arg, (old_chunk)); \
+      (*(h)->freefun.fn_extra_arg) ((h)->extra_arg, (old_chunk)); \
     else \
-      (*(void (*) (void *)) (h)->freefun) ((old_chunk)); \
+      (*(h)->freefun.fn) ((old_chunk)); \
   } while (0)
 
 
@@ -159,8 +159,8 @@ _obstack_begin (struct obstack *h,
       size = 4096 - extra;
     }
 
-  h->chunkfun = (struct _obstack_chunk * (*)(void *, long)) chunkfun;
-  h->freefun = (void (*) (void *, struct _obstack_chunk *)) freefun;
+  h->chunkfun.fn = chunkfun;
+  h->freefun.fn = freefun;
   h->chunk_size = size;
   h->alignment_mask = alignment - 1;
   h->use_extra_arg = 0;
@@ -206,8 +206,9 @@ _obstack_begin_1 (struct obstack *h, int size, int alignment,
       size = 4096 - extra;
     }
 
-  h->chunkfun = (struct _obstack_chunk * (*)(void *,long)) chunkfun;
-  h->freefun = (void (*) (void *, struct _obstack_chunk *)) freefun;
+  h->chunkfun.fn_extra_arg = (struct _obstack_chunk * (*)(void *,long)) chunkfun;
+  h->freefun.fn_extra_arg = (void (*) (void *, struct _obstack_chunk *)) freefun;
+
   h->chunk_size = size;
   h->alignment_mask = alignment - 1;
   h->extra_arg = arg;
